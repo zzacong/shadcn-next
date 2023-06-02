500e2f8dd9d437dcc26607e0c0d76dc5133c2720
Zac Ong <61817066+zzacong@users.noreply.github.com>
2023-06-02 11:20:57 +1000

    t3

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  semi: true,
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+};
+
+module.exports = config;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from 'tailwindcss';
+
+export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,4 +16,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+} satisfies Config;
